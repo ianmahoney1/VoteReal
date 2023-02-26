39d4e49d3897db41cf47ceeae526e07d349857cb
samuellevy <slevy3@uw.edu>
2023-02-26 07:31:00 -0500

    make report work :)

--- src/components/Report.js
+++ src/components/Report.js
@@ -9,7 +9,14 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import BillService from '../services/BillService'; 
 
 function ReportScreen({ route, navigation }) {
-  // const userObj = route.params;  
+  const billVote = route.params;  
+  if (billVote == null) {
+      billVote = {
+          name: "CB 120374",
+          vote: "Approve",
+          summary: "This ordinance adds protections against discrimination based on an individual's actual, potential, perceived, or alleged pregnancy outcomes in Seattle Municipal Code. The ordinance responds to the U.S. Supreme Court's decision in Dobbs v. Jackson Women's Health Organization, which overruled the constitutional right to an abortion established by Roe v. Wade and Planned Parenthood of Southeastern Pa. v. Casey. The ordinance acknowledges the Reproductive Privacy Act of Washington and the disproportionate impact that restricting and/or denying access to abortion services will have on poor communities and BIPOC communities. The ordinance recognizes the fundamental right to bodily autonomy, including for transgender and gender diverse communities who face increased barriers and stigma when accessing abortion services, and commits to a city that does not subject its residents, workers, and visitors to discriminatory treatment."
+      }
+  }
   // const stats = BillService.getStats(userObj); 
   
   return (
@@ -17,22 +24,18 @@ function ReportScreen({ route, navigation }) {
         <Card style={{alignItems: 'center'}}>
             <Card.Content style={{alignItems: 'center'}}>
             <Card.Title
-            text={"Bill: CB 120374"}
+            text={billVote.name}
             />
-            <Text styles={styles.baseText}>
-                <Ionicons name="md-checkmark-circle" size={15} color="green" /> 
-                You voted to Approve this bill
+            <Text styles={styles.baseText}>   
+                    {billVote.vote == "Approve" && <Text><Ionicons name="md-checkmark-circle" size={15} color="green" /> You voted to Approve this bill</Text>}
+                    {billVote.vote != "Approve" && <Text><Ionicons name="alert-circle" size={15} color="red" /> You voted to Reject this bill</Text>}
             </Text>
             <Text></Text>
             <Card.Row>
                     <Text>
-                    This ordinance adds protections against discrimination based on an individual's actual, potential, perceived, or alleged pregnancy outcomes in Seattle Municipal Code.
-                    The ordinance responds to the U.S. Supreme Court's decision in Dobbs v. Jackson Women's Health Organization, which overruled the constitutional right to an abortion established by Roe v. Wade and Planned Parenthood of Southeastern Pa. v. Casey.
-                    The ordinance acknowledges the Reproductive Privacy Act of Washington and the disproportionate impact that restricting and/or denying access to abortion services will have on poor communities and BIPOC communities.
-                    The ordinance recognizes the fundamental right to bodily autonomy, including for transgender and gender diverse communities who face increased barriers and stigma when accessing abortion services, and commits to a city that does not subject its residents, workers, and visitors to discriminatory treatment.
+                        {billVote.summary}
                     </Text>
             </Card.Row>
-
         </Card.Content>
         </Card>
     </View>
