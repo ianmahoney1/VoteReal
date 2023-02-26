d989896f1d7c821fa05d9602cd90c09e6b466301
ianmahoney1 <42852481+ianmahoney1@users.noreply.github.com>
2023-02-26 00:14:16 -0500

    Authentication works

--- App.js
+++ App.js
@@ -37,23 +37,23 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center',
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3',
-  },
-  buttonText: {
-    textAlign: 'center',
-    padding: 20,
-    color: 'black',
-  },
-});
+//const styles = StyleSheet.create({
+//  container: {
+//    paddingTop: 60,
+//    alignItems: 'center',
+//  },
+//  button: {
+//    marginBottom: 30,
+//    width: 260,
+//   alignItems: 'center',
+//    backgroundColor: '#2196F3',
+//  },
+//  buttonText: {
+//    textAlign: 'center',
+//    padding: 20,
+//    color: 'black',
+//  },
+//});
 
 
       // {/* <Button
